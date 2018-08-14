8c90e1993b4f4c2c5561ce42e425476342762916
Mark Sarkis <mark.sarkis93@gmail.com>
2018-08-13 19:53:31 -0700

    added key property to list

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 class App extends Component {
   state = {
     persons : [
-      {name: 'Mark', age:25},
-      {name: 'Alex', age:24},
-      {name: 'Daniel', age:30}
+      {id:'1kd', name: 'Mark', age:25},
+      {id:'2skd', name: 'Alex', age:24},
+      {id:'3fdd', name: 'Daniel', age:30}
     ],
     showPersons: true
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons : [
-        {name: 'Mark', age:25},
-        {name: 'Alex', age:24},
-        {name: event.target.value, age:30}
-      ]
-    })
-  }
+  // nameChangedHandler = (event) => {
+  //   this.setState({
+  //     persons : [
+  //       {name: 'Mark', age:25},
+  //       {name: 'Alex', age:24},
+  //       {name: event.target.value, age:30}
+  //     ]
+  //   })
+  // }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({
       persons: persons
@@ -56,15 +56,16 @@ togglePersonsHandler = () => {
           return <Person
           click={() => this.deletePersonHandler(index)}
           name={person.name}
-          age={person.age}/>
+          age={person.age}
+          key={person.id}/>
         })}
       </div>
     )}
 
     return (
       <div className="App">
-        <h1> Figuring out State and Stateless components </h1>
-        <p> Click on a the first line of each to delete the person </p>
+        <h1> Improving Knowledge about State/less components </h1>
+        <p> Click on the first line of each to delete the person </p>
         {persons}
         <button style = {style}
             onClick={this.togglePersonsHandler}>
