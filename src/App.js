c2f07eda33b92016362c81f1335159749cb74a91
Mark Sarkis <mark.sarkis93@gmail.com>
2018-08-13 21:02:04 -0700

    created dynamic access in the input

--- src/App.js
+++ src/App.js
@@ -12,15 +12,21 @@ class App extends Component {
     showPersons: true
   }
 
-  // nameChangedHandler = (event) => {
-  //   this.setState({
-  //     persons : [
-  //       {name: 'Mark', age:25},
-  //       {name: 'Alex', age:24},
-  //       {name: event.target.value, age:30}
-  //     ]
-  //   })
-  // }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+        ...this.state.persons[personIndex]
+    };
+    person.name  = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons:persons})
+  }
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
@@ -54,10 +60,11 @@ togglePersonsHandler = () => {
         <div>
           {this.state.persons.map((person, index) => {
           return <Person
-          click={() => this.deletePersonHandler(index)}
-          name={person.name}
-          age={person.age}
-          key={person.id}/>
+          click = {() => this.deletePersonHandler(index)}
+          name = {person.name}
+          age = {person.age}
+          key = {person.id}
+          changed = {(event) => this.nameChangedHandler(event, person.id)} />
         })}
       </div>
     )}
