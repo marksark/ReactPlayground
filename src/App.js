58f490b0b847ba227c5890c61478cc26f08c1e60
Mark Sarkis <mark.sarkis93@gmail.com>
2018-09-20 20:21:03 -0700

    cleaned up styling

--- src/App.js
+++ src/App.js
@@ -12,17 +12,21 @@ class App extends Component {
     showPersons: true
   }
 
+  //change name of person whose input box is engaged
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
     const person = {
+
         ...this.state.persons[personIndex]
     };
+
     person.name  = event.target.value;
 
     const persons = [...this.state.persons];
+
     persons[personIndex] = person;
 
     this.setState({persons:persons})
@@ -36,6 +40,7 @@ class App extends Component {
     });
   }
 
+//toggle showing and hiding list of people
 togglePersonsHandler = () => {
   const doesShow = this.state.showPersons;
   this.setState({
@@ -43,41 +48,38 @@ togglePersonsHandler = () => {
   });
 }
 
-  render() {
 // everything under render re-renders everytime the state is changed
-    const style = {
-      backgroundColor: 'white',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+  render() {
 
     let persons = null;
 
+    //how to show all persons without calling individually
     if (this.state.showPersons) {
-
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-          return <Person
-          click = {() => this.deletePersonHandler(index)}
-          name = {person.name}
-          age = {person.age}
-          key = {person.id}
-          changed = {(event) => this.nameChangedHandler(event, person.id)} />
-        })}
-      </div>
+            return <Person
+            click = {() => this.deletePersonHandler(index)}
+            name = {person.name}
+            age = {person.age}
+            key = {person.id}
+            changed = {(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}
+        </div>
     )}
 
     return (
+      //try to keep components under this minimal
       <div className="App">
         <h1> Improving Knowledge about State/less components </h1>
         <p> Click on the first line of each to delete the person </p>
         {persons}
-        <button style = {style}
+        <button
+          className = "buttonSelected"
             onClick={this.togglePersonsHandler}>
             Toggle All Persons
-            </button>
+        </button>
       </div>
     );
   }
