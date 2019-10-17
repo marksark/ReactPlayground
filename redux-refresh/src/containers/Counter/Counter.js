a0eaefe77a4ec2c9c72d1c3cc8c531e6dab72f08
Mark Sarkis <msarkis@211la.org>
2019-10-17 09:15:49 -0700

    updated redux-refresh

--- redux-refresh/src/containers/Counter/Counter.js
+++ redux-refresh/src/containers/Counter/Counter.js
@@ -1,29 +1,14 @@
+import * as actionTypes from '../../store/actions';
+
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import { connect } from 'react-redux';
+
 class Counter extends Component {
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
     render () {
-        // console.log(this.props)
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
